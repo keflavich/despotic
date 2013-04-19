f8fae53ac347cac1b12dce7e661b193a084c0eaa
Adam Ginsburg <keflavich@gmail.com>
2013-04-19 17:57:31 -0600

    need a setup.py file...

---
+++ setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+import sys
+from distutils.core import setup, Command
+import subprocess
+if 'develop' in sys.argv:
+    # use setuptools for develop, but nothing else
+    from setuptools import setup
+
+with open('README.md') as file:
+    long_description = file.read()
+
+with open('CHANGES') as file:
+    long_description += file.read()
+
+with open('REQUIREMENTS') as file:
+    requirements = file.readlines()
+
+try:  # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError:  # Python 2.x
+    from distutils.command.build_py import build_py
+
+
+from despotic import __version__ as version
+tagname = "despotic_%s" % (version)
+
+download_url = "https://despotic.googlecode.com/svn/trunk/dist/DESPOTIC-%s.tar.gz" % version
+
+class PyTest(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, 'tests/run_tests.py'])
+        raise SystemExit(errno)
+
+setup(name='despotic',
+      version=version,
+      description='a Python / numPy / sciPy package to perform calculations related to line emission and thermal behavior in cold interstellar clouds.',
+      long_description=long_description,
+      author=['Mark Krumholz'],
+      author_email=['mkrumhol@ucsc.edu'], 
+      url='https://code.google.com/p/despotic/',
+      download_url=download_url,
+      packages=['despotic'],
+      package_dir={'despotic':'despotic'}, 
+      package_data={'despotic.cloudfiles':['cloudfiles/']},
+      requires=requirements,
+      cmdclass={'build_py': build_py, 'test': PyTest},
+      classifiers=[
+                   "Development Status :: 3 - Alpha",
+                   "Programming Language :: Python",
+                   "License :: OSI Approved :: GPL",
+                  ],
+      
+     )
+
