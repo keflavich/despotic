e63b8980cf22f10fda38d66e445e56ee5af8b303
Adam Ginsburg <keflavich@gmail.com>
2013-04-20 11:33:14 -0600

    include timing output

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,13 +1,15 @@
 import time
 
+# run all examples except RADEX
 # skip this one execfile('examples/radexComp.py')
 
-# run all examples except RADEX
-
-t0 = time.time()
+# keep track of timing
+start_time = time.time()
 for fn in ['coSLED','coreTemp','formaldehyde_tests','matrixCondition','pCygni','shockCool']:
 
-    print "Running {0}...".format(fn),
+    this_time = time.time()
+    print "Running {0}... started at {1}".format(fn,start_time)
     execfile('examples/{0}.py'.format(fn))
-    print "Completed {0} in {1} seconds".format(fn,time.time()-t0)
+    print "Completed {0} in {1} seconds.  Elapsed time is {2} seconds.".format(fn,
+            time.time()-this_time, time.time()-start_time)
     print
