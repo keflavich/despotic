32ca3f8248f156ab7e481247d2b6158f61a6b80a
Adam Ginsburg <keflavich@gmail.com>
2013-04-20 11:25:45 -0600

    trivial bugfix

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,7 +5,7 @@ import time
 # run all examples except RADEX
 
 t0 = time.time()
-for fn in 'coSLED','coreTemp','formaldehyde_tests','matrixCondition','pCygni','shockCool']:
+for fn in ['coSLED','coreTemp','formaldehyde_tests','matrixCondition','pCygni','shockCool']:
 
     print "Running {0}...".format(fn),
     execfile('examples/{0}.py'.format(fn))
