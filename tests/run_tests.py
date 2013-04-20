7d51f0b7296a3437611ebc67872e42409c36acb8
Adam Ginsburg <keflavich@gmail.com>
2013-04-19 19:16:16 -0600

    really?  forgot to add tests, apparently...

---
+++ tests/run_tests.py
@@ -0,0 +1,8 @@
+# run all examples except RADEX
+execfile('examples/coSLED.py')
+execfile('examples/coreTemp.py')
+execfile('examples/formaldehyde_tests.py')
+execfile('examples/matrixCondition.py')
+execfile('examples/pCygni.py')
+# skip this one execfile('examples/radexComp.py')
+execfile('examples/shockCool.py')
