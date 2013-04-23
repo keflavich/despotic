58c317b44c27dd78caabce8fa8ac9cc908732641
Adam Ginsburg <keflavich@gmail.com>
2013-04-22 22:36:35 -0600

    some requirement and test updates

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,7 +9,13 @@ for fn in ['coSLED','coreTemp','formaldehyde_tests','matrixCondition','pCygni','
 
     this_time = time.time()
     print "Running {0}... started at {1}".format(fn,start_time)
-    execfile('examples/{0}.py'.format(fn))
+    scriptfile = '{0}.py'.format(fn)
+    if os.path.exists('tests/'+scriptfile):
+        execfile('tests/'+scriptfile)
+    elif os.path.exists('examples/'+scriptfile):
+        execfile('examples/'+scriptfile)
+    else:
+        raise IOError("Missing script file for {0}".format(str(fn)))
     print "Completed {0} in {1} seconds.  Elapsed time is {2} seconds.".format(fn,
             time.time()-this_time, time.time()-start_time)
     print
